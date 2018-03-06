20c631bff84792c02f5074eb68096fdba5554dcc
Arturo Guerra <arturoguerra360@gmail.com>
2018-03-06 13:58:57 -0600

    First commit

---
+++ sshconfig.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.6
+import subprocess
+import re
+
+from pathlib import Path
+
+configFile = str(Path.home()) + '/.ssh/config'
+blacklist = ['auttaja', 'osu', 'mandalore']
+sshconfig = open(configFile, 'r')
+
+def ssh_genlist():
+    l = list()
+    lines = sshconfig.readlines()
+    for line in lines:
+        regex = re.match("Host ((?!\*)\w+)", line)
+        if (regex) and regex.group(1) not in blacklist:
+            l.append(regex.group(1))
+    return l
+
+for s in ssh_genlist():
+    print(f"Server Name: {s}")
+    cmd = s + ':.ssh/config'
+    subprocess.call(['scp', configFile, f'{cmd}'])
